83617f8259ee003984201a33a394af433f9717e2
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-02 00:02:07 +0200

    Card Game Flush

--- Chapter_10/CardLib/Client/Program.cs
+++ Chapter_10/CardLib/Client/Program.cs
@@ -2,16 +2,58 @@
 
 Deck myDeck = new Deck();
 myDeck.Shuffle();
-for (int i = 0; i < 52; i++)
+int j = 0;
+while (j < 1)
 {
 
-    Card tempCard = myDeck.GetCard(i);
-    Console.Write(tempCard.ToString());
 
-    if (i != 51)
-        Console.Write(",\n");
-    else
-        Console.WriteLine();
+
+    for (int i = 0; i < 5; i++)
+    {
+        Random rand = new Random();
+
+        var five = rand.Next(4);
+
+        Card tempCard = myDeck.GetCard(five);
+
+        Console.WriteLine(tempCard);
+
+
+        var tempIndex = (int)tempCard.suit;
+
+
+        Console.WriteLine(tempCard.suit);
+
+        Console.WriteLine(tempIndex);
+
+        if (tempIndex.Equals(tempCard.suit))
+        {
+
+            Console.WriteLine("Flush");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
+        j++;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    //if (i != 51)
+    //    Console.Write(",\n");
+    //else
+    //    Console.WriteLine();
 }
 Console.ReadKey();
 
