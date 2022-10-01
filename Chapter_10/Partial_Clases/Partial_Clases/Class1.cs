f99d34fae6ad610abb9b587e2a5e6484310e6cd6
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-01 18:38:46 +0200

    01.2022

---
+++ Chapter_10/Partial_Clases/Partial_Clases/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace Partial_Clases
+{
+    public class MyClass
+    {
+
+        #region Fields
+        private int myInt;
+        #endregion Fields
+
+
+        #region Constructor
+        public MyClass() { myInt = 99; }
+        #endregion
+
+        #region Properties
+        public int MyInt
+        {
+
+            get { return myInt; }
+            set { myInt = value; }
+        }
+        #endregion
+        #region Methods
+        public void DoSomething()
+        {
+            //Do something
+        }
+        #endregion
+
+
+    }
+}
