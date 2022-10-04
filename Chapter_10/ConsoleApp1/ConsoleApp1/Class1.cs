7891afb8697a85f90b30b283f4702c8b6761b148
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-04 19:03:55 +0200

    Card Flush

---
+++ Chapter_10/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public readonly Enum1 enum1;
+
+        public Class1()
+        {
+        }
+
+        public Class1(Enum1 newEnum1)
+        {
+
+            Enum1 = newEnum1;
+
+        }
+
+        public Enum1 Enum1 { get; }
+    }
+}
---
+++ Chapter_10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using ConsoleApp1;
+
+Class1 test = new Class1();
+
+var test1 = (int)test.enum1;
+
+Console.WriteLine((test1)1);
+
+
+
