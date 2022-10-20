129a4dec200e38ae7be41687ee29efc39c6e7e87
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-20 20:31:02 +0200

    BeginningCSharpAndDotNET_16_6_LargeNumber- Query

---
+++ Chapter16/BeginningCSharpAndDotNET_16_5_OrderQueryResults/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small",
+"Ruiz", "Hsieh", "Jorgenson", "Ilyich", "Singh", "Samba", "Fatimah" };
+
+var result = from n in names where n.StartsWith("S") orderby n.Length select n;
+
+foreach (var name in result)
+{
+
+    Console.WriteLine(name);
+}
+
+
+IEnumerable<string> result1 = names.Where(n => n.StartsWith("S")).OrderBy(n => n.Length);
+
+foreach (var name in result1)
+{
+    Console.WriteLine(name);
+}
---
+++ Chapter16/BeginningCSharpAndDotNET_16_6_LargeNumber-/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] numbers = GenerateLotsOfNumbers(12045678);
+
+var query = from n in numbers where n < 1000 select n;
+
+Console.WriteLine();
+
+foreach (var n in query)
+{
+	Console.WriteLine(n);
+
+}
+
+int[] GenerateLotsOfNumbers(int lenght)
+{
+	Random random = new Random();
+	int[] numbers = new int[lenght];
+	for (int i = 0; i < lenght; i++)
+	{
+		numbers[i] = random.Next(0, lenght);
+
+	}
+
+	return numbers;
+
+}
