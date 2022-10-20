49504f1c629ef835ca406e6e9f4127f39f50a49f
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-20 20:45:03 +0200

    BeginningCSharpAndDotNET_16_7_NumericAggregates

---
+++ Chapter16/BeginningCSharpAndDotNET_16_7_NumericAggregates/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[] numbers = GenerateLotsOfNumbers(12045678);
+
+var queryReseul = from n in numbers where n > 1000 select n;
+
+Console.WriteLine("Count of Numbers > 1000");
+Console.WriteLine(queryReseul.Count());
+Console.WriteLine("Max of Numbers >1000");
+Console.WriteLine(queryReseul.Max());
+Console.WriteLine("Min of Numbers >1000");
+Console.WriteLine(queryReseul.Min());
+Console.WriteLine("Average of Numbers>1000");
+Console.WriteLine(queryReseul.Average());
+Console.WriteLine("Sum of Numbers >1000");
+Console.WriteLine(queryReseul.Sum(n => (long)n));
+Console.Write("Program finished, press Enter/Return to continue:");
+
+
+
+int[] GenerateLotsOfNumbers(int count)
+{
+    Random generator = new Random(0);
+    int[] result = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        result[i] = generator.Next();
+    }
+    return result;
+}
