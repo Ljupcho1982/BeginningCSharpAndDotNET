7284ccaebce686c56386806b6d1f896786d4a082
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-20 19:06:07 +0200

    BeginningCSharpAndDotNET_16_3_QuerySyntax

---
+++ Chapter16/BeginningCSharpAndDotNET_16_3_QuerySyntax/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small", "Ruiz", "Hsieh", "Jorgenson", "Ilych", "Singh", "Samba", "Fatima" };
+
+var querryResults = from n in names where n.StartsWith("S") select n;
+
+Console.WriteLine("Names beginning with S: ");
+
+foreach (var item in querryResults)
+{
+    Console.WriteLine(item);
+}
+
+Console.Write("Program finished, press Enter/Return to continue: ");
+
+IEnumerable<string> methodQuery = names.Where(n => n.StartsWith("S"));
+
