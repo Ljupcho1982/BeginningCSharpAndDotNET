130c06c4eb01d03100c7dbe4a204c14479a65dd8
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-21 04:36:06 +0200

    orderby

--- Chapter16/BeginningCSharpAndDotNET_16_6_LargeNumber-/Program.cs
+++ Chapter16/BeginningCSharpAndDotNET_16_6_LargeNumber-/Program.cs
@@ -1,13 +1,17 @@
 ﻿
 int[] numbers = GenerateLotsOfNumbers(12045678);
 
-var query = from n in numbers where n < 1000 select n;
+
+
+var query = from n in numbers where n < 1000 orderby n select n;
 
 Console.WriteLine();
 
 foreach (var n in query)
 {
+
 	Console.WriteLine(n);
+	Console.WriteLine(Convert.ToInt64(n));
 
 }
 
