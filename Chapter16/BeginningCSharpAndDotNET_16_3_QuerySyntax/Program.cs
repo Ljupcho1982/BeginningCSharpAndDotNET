d010abd1dc7c66ca92f627c8f53a6c0c0b4f78f9
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-21 04:01:17 +0200

    Reverse order

--- Chapter16/BeginningCSharpAndDotNET_16_3_QuerySyntax/Program.cs
+++ Chapter16/BeginningCSharpAndDotNET_16_3_QuerySyntax/Program.cs
@@ -7,9 +7,20 @@ Console.WriteLine("Names beginning with S: ");
 foreach (var item in querryResults)
 {
     Console.WriteLine(item);
+    // Console.WriteLine($"Orderby in descending order {item.Reverse()}");
 }
+
+
+Console.WriteLine("-----------Orderby in descending order------------");
+foreach (var item in querryResults.Reverse())
+{
+
+    Console.WriteLine(item);
+}
+
 
 Console.Write("Program finished, press Enter/Return to continue: ");
 
 IEnumerable<string> methodQuery = names.Where(n => n.StartsWith("S"));
+
 
