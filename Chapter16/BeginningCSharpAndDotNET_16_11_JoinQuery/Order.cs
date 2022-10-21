3f6ccbcf35ac40ffc1b7e1dedb9d1b279ed9f307
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-10-21 19:54:32 +0200

    join

---
+++ Chapter16/BeginningCSharpAndDotNET_16_11_JoinQuery/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace BeginningCSharpAndDotNET_16_11_JoinQuery
+{
+    internal class Order
+    {
+
+        public string ID { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
